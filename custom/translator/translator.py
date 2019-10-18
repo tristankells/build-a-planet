71e0b4a6faf8e3387400d2e3b814b0e9aa6fcf79
Anthony <anthonyw1@protonmail.com>
2019-10-19 11:14:03 +1300

    Translator with star and planet prompts
    Translator with star and planet prompts

---
+++ custom/translator/translator.py
@@ -0,0 +1,10 @@
+class Translator:
+    class Star:
+        star_brightness = "How bright is the star?"
+        star_age = "How old is the star?"
+        star_size = "How large is the star?"
+
+    class Planet:
+        planet_size = "What size is your planet? Small, medium, or large?"
+        planet_distance = "How far away is your planet from the star?"
+        planet_atmosphere = "What atmospheric condition is your planet?"
