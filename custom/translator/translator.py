acce868449e67ecc48acd3cc01864a216d612af8
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2019-10-19 19:25:09 +1300

    Added script to translator

--- custom/translator/translator.py
+++ custom/translator/translator.py
@@ -2,31 +2,41 @@ class Translator:
     class Launch:
         launch = "Welcome to Build a Planet!"
 
-    class Star:
-        star_brightness = "How bright is the star? White, red, or blue?"
-        star_brightness_white = "star is white"
-        star_brightness_blue = "star is blue"
-        star_brightness_red = "star is red"
+    class Solar_System:
+        planetary_system_create = "To get started, let’s build a planetary system! Do you want to learn more about planetary systems?"
+        planetary_system_yes = "Awesome, here’s what you need to know.A planetary system contains a star (like our sun) with a set of gravitationally bound objects in orbit around it (like the planets in our solar system).As the creator of the universe, your choices will determine what kind of planet you’ll build… will it be a new earth? An icy rock? A gaseous giant? Your planet story awaits..."
+        planetary_system_no = "Awesome, Let’s create your planetary system."
 
-        star_size = "How large is the star? Dwarf, giant, or super?"
-        star_size_dwarf = "star is dwarf"
-        star_size_giant = "star is giant"
-        star_size_super = "star is super"
+    class Star:
+        star_create = "Whould you like to create a star?"
+        star_brightness = "What is the brightness of your star?"
+        star_brightness_blue = "Blue stars are super hot. The temperature is between 7 to 30 million degrees celsius.These are the hottest stars in the known universe. An example of a blue star is Sirius, located in the Vega constellation."
+        star_brightness_yellow = "Yellow stars are hot. The temperature is between 5-6 million degrees celsius.These stars are fairly hot and bright. An example is our own sun."
+        star_brightness_red = "Red stars are Warm. The temperature is between 2-4 Million degrees celsius.These stars are colder than most and are far less brighter. An example of this is Betelgeuse in the Antares constellation."
+        star_brightness_other = "This is not a valid brightness. You have to choose between Blue, Yellow or Red."
+
+        star_size = "How large is the star? Dwarf, giant, or super giant?"
+        star_size_dwarf = ""
+        star_size_giant = ""
+        star_size_super = ""
 
         star_age = "How old is the star? Middleaged, old, or young?"
-        star_age_middleaged = "star is middle aged"
-        star_age_old = "star is old"
-        star_age_young = "star is young"
+        star_age_young = ""     
+        star_age_middleaged = ""
+        star_age_old = ""
+        
 
     class Planet:
-        planet_size = "What size is your planet? Small, medium, or large?"
-        planet_size_large = "planet is large"
-        planet_size_small = "planet is small"
-        planet_size_medium = "planet is medium"
+        planet_create = "Whould you like to create a planet"
 
-        planet_distance = "How far away is your planet from the star? near, midway, or far?"
-        planet_distance_near = "Planet is near "
-        planet_distance_midway = "Planet is midway"
-        planet_distance_far = "Planets is far "
+        planet_size = "What size is your planet? Small, medium, or large?"
+        planet_size_large = ""
+        planet_size_small = ""
+        planet_size_medium = ""
+
+        planet_distance = "How far away is your planet from the star? Close, midway, or far?"
+        planet_distance_near = "Planets that are near blah blah blah"
+        planet_distance_midway = "Planets that are near blah blah blah"
+        planet_distance_far = "Planets that are near blah blah blah"
 
         planet_atmosphere = "What atmospheric condition is your planet? Hydrogen, nitrogen, or oxygen?"
