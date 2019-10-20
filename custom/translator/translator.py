5065299bef679e93bdb711d2c8492238e491853d
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2019-10-20 13:19:11 +1300

    added fact class in translator

--- custom/translator/translator.py
+++ custom/translator/translator.py
@@ -52,3 +52,11 @@ class Translator:
         game_end = "Awesome! You have created a planetary system! Do you want to learn more about it?"
         game_end_yes = "Displaying your planet with the stars"
         game_end_no = "Thanks for playing"
+
+    class Facts:
+        fact_create = "Do you want to know a fact about space?"
+        facts = [
+                    'There is no atmosphere in space, which means that sound has no medium or way to travel to be heard. Astronauts use radios to stay in communication while in space, since radio waves can still be sent and received.'
+                    'Venus is the hottest planet in the solar system and has an average surface temperature of around 450° C. Interestingly, Venus is not the closest planet to the Sun – Mercury is closer but because Mercury has no atmosphere to regulate temperature it has a very large temperature fluctuation.'
+                    'Of all the planets in our solar system apart from Earth, Mars is the one most likely to be hospitable to life. In 1986, NASA found what they thought may be fossils of microscopic living things in a rock recovered from Mars. '
+                ]
