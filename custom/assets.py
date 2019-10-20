6572b6df8f4771781942e0ae9e77bbd2447f681c
Anthony <anthonyw1@protonmail.com>
2019-10-20 15:06:58 +1300

    Colour restructure

--- custom/assets.py
+++ custom/assets.py
@@ -1,8 +1,9 @@
 class Assets:
     class Pictures:
-        RED_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/red_star.png'
-        BLUE_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/blue_star.png'
-        YELLOW_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/yellow_star.png'
+        RED_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/Stars/Red/red_star.png'
+        BLUE_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/Stars/Blue/blue_star.png'
+        YELLOW_BRIGHTNESS = 'https://planet-story.s3.amazonaws.com/Stars/Yellow/yellow_star.png'
+
         BLUE_DWARF = 'https://planet-story.s3.amazonaws.com/blue_dwarf.png'
         BLUE_GIANT = 'https://planet-story.s3.amazonaws.com/blue_giant.png'
         BLUE_SUPER_GIANT = 'https://planet-story.s3.amazonaws.com/blue_super_giant.png'
