7fc8b49e42cd5d58c8ce170009244242d1fa0eff
Anthony <anthonyw1@protonmail.com>
2019-10-20 14:59:46 +1300

    Fix blue star links

--- custom/assets.py
+++ custom/assets.py
@@ -7,6 +7,15 @@ class Assets:
         BLUE_GIANT = 'https://planet-story.s3.amazonaws.com/blue_giant.png'
         BLUE_SUPER_GIANT = 'https://planet-story.s3.amazonaws.com/blue_super_giant.png'
         
+        BLUE_YOUNG_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-dwarf.png'
+        BLUE_YOUNG_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-giant.png'
+        BLUE_YOUNG_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-super.png'
+        BLUE_MIDDLE_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-dwarf.png'
+        BLUE_MIDDLE_GIANT = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-giant.png'
+        BLUE_MIDDLE_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-super.png'
+        BLUE_OLD_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-dwarf.png'
+        BLUE_OLD_GIANT = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-giant.png'
+        BLUE_OLD_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-super.png'
 
 
         YELLOW_YOUNG_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Yellow/Young/yellow-young-dwarf.png'
@@ -18,15 +27,3 @@ class Assets:
         YELLOW_OLD_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Yellow/Old/yellow-old-dwarf.png'
         YELLOW_OLD_GIANT = 'https://planet-story.s3.amazonaws.com/Stars/Yellow/Old/yellow-old-giant.png'
         YELLOW_OLD_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Yellow/Old/yellow-old-super.png'
-
-        BLUE_MIDDLE_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-dwarf.png'
-        BLUE_MIDDLE_GIANT = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-giant.png'
-        BLUE_MIDDLE_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Middle/blue-middle-super.png'
-
-        BLUE_OLD_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-dwarf.png'
-        BLUE_OLD_GIANT = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-giant.png'
-        BLUE_MIDDLE_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Old/blue-old-super.png'
-
-        BLUE_YOUNG_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-dwarf.png'
-        BLUE_YOUNG_DWARF = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-giant.png'
-        BLUE_YOUNG_SUPER = 'https://planet-story.s3.amazonaws.com/Stars/Blue/Young/blue-young-super.png'
