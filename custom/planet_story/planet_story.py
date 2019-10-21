ebe76affa693b52c40d8461c20a2374bf61861fc
tristankells <tristankells@gmail.com>
2019-10-21 19:00:41 +1300

    Removed overriding line

--- custom/planet_story/planet_story.py
+++ custom/planet_story/planet_story.py
@@ -122,7 +122,7 @@ class PlanetStory:
 
     def review_solar_system(self):
         self.speech_text += self.planet_story
-        self.speech_text = Translator.End_Game.game_play_again_yes
+        self.speech_text += Translator.End_Game.game_play_again_yes
         self.speech_text += Translator.Star.star_brightness
 
         self.current_question = Question.Star.STAR_BRIGHTNESS
